3544c4161d0a27bfb9ecf6915a0e130b3cf6c4ea
Yusuf Hartanto <yusuf.hartanto69@gmail.com>
2025-04-16 21:44:05 +0700

    update:portfolio - properties filter by client_id

--- src/module/portfolio/properties/properties.controller.ts
+++ src/module/portfolio/properties/properties.controller.ts
@@ -42,8 +42,11 @@ const uploadImages = async (req: Request) => {
 export default class Controller {
   public async list(req: Request, res: Response) {
     try {
+      const clientId: any = req?.query?.client_id;
+
       let condition: any = {};
-      if (!['administrator', 'agent'].includes(req?.user?.role_name))
+      if (clientId != undefined) condition = { properties_holder: clientId };
+      else if (!['administrator', 'agent'].includes(req?.user?.role_name))
         condition = { properties_holder: req?.user?.client_id };
 
       const result = await repository.list(condition);
@@ -65,9 +68,11 @@ export default class Controller {
       const limit: any = req?.query?.perPage || 10;
       const offset: any = req?.query?.page || 1;
       const keyword: any = req?.query?.q;
+      const clientId: any = req?.query?.client_id;
 
       let condition: any = {};
-      if (!['administrator', 'agent'].includes(req?.user?.role_name))
+      if (clientId != undefined) condition = { properties_holder: clientId };
+      else if (!['administrator', 'agent'].includes(req?.user?.role_name))
         condition = { properties_holder: req?.user?.client_id };
 
       const { count, rows } = await repository.index({
