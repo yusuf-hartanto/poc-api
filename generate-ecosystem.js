607a51697962f2ef5c191684e5dae04e29c89c5e
Yusuf Hartanto <yusuf.hartanto69@gmail.com>
2025-07-24 22:31:20 +0700

    add generate ecosystem

---
+++ generate-ecosystem.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name : 'api',
+    script: './dist/apps/api/server.js',
+    node_args: "-r dotenv/config",
+    args: ["dotenv_config_path=./.env.stage.dev"],
+  }
+]};
+
+console.log("✅ ecosystem.config.js generated");
